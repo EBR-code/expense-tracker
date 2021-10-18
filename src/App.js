91ea2a2b422decb0e78d7c0e5eed529426c1294e
Edson Ragas <ragasedson@gmail.com>
2021-10-19 07:00:59 +0800

    add ExpensesList component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import ExpenseItem from './components/ExpenseItem';
+// App.js is called into index.js on the ReactDOM.render(<App />) line at the location within index.html (in the public folder that has the div with an id of root)
+
+import ExpensesList from './components/ExpensesList';
 
 function App() {
+  // temporary expense data for testing purposes.
   const expenses = [
     {
       id: 'e1',
@@ -25,27 +28,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <ExpensesList expenses={expenses} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+// calls the app.js file which defines and exports the App component.
+
 import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-
 ReactDOM.render(<App />, document.getElementById('root'));
