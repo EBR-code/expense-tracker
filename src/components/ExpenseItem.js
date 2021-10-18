40383722613820609e5b1448c8a920629e31c718
Edson Ragas <ragasedson@gmail.com>
2021-10-19 04:43:46 +0800

    update date display

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,19 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const year = props.date.toLocaleString('en-US', { year: 'numeric' });
+  const day = props.date.toLocaleString('en-US', { day: 'numeric' });
   return (
     <div className="expense-item">
-      <div>October 18 2021</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>Food</h2>
-        <div className="expense-item__price">$100</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">$ {props.amount}</div>
       </div>
     </div>
   );
