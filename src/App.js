27d2888beb3a12d95336f40f1d857cd32b8ba15b
Edson Ragas <ragasedson@gmail.com>
2021-10-22 01:43:56 +0800

    organize component file, update imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js is called into index.js on the ReactDOM.render(<App />) line at the location within index.html (in the public folder that has the div with an id of root)
 import React from 'react';
-import ExpensesList from './components/ExpensesList';
+import ExpensesList from './components/Expenses/ExpensesList';
 
 const App = () => {
   // temporary expenses data for testing purposes.
