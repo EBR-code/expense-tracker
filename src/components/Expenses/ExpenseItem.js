31f894b1a6c7f0f75cbc75c1823fb7a06489d0ee
Edson Ragas <ragasedson@gmail.com>
2021-10-24 03:11:31 +0800

    add react hooks

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,28 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = props => {
+  useState();
+
+  let title = props.title;
+
+  const clickHandler = () => {
+    title = 'Updated!';
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
       </div>
+      {/* onClick adds an event listener, to a click event in this button */}
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
