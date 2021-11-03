2566b17194c40632b276e28dc2d47fea8046b9eb
Edson Ragas <ragasedson@gmail.com>
2021-11-03 22:19:33 +0800

    add stateful list, rendered list dynamically

--- src/App.js
+++ src/App.js
@@ -1,47 +1,50 @@
 // App.js is called into index.js on the ReactDOM.render(<App />) line at the location within index.html (in the public folder that has the div with an id of root)
-import React from 'react';
+import React, { useState } from 'react';
 import ExpensesList from './components/Expenses/ExpensesList';
 import NewExpense from './components/NewExpenses/NewExpense';
 
+// temporary expenses data for testing purposes.
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14)
+  },
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 799.49,
+    date: new Date(2021, 2, 12)
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28)
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12)
+  }
+];
+
 const App = () => {
-  // temporary expenses data for testing purposes.
-  const expensesArray = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14)
-    },
-    {
-      id: 'e2',
-      title: 'New TV',
-      amount: 799.49,
-      date: new Date(2021, 2, 12)
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28)
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12)
-    }
-  ];
-  //function we passed through a prop to get expenseData from child component NewExpense which it also got from its child component ExpenseForm.
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  // function handler
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses(prevExpense => {
+      return [expense, ...prevExpense];
+    });
   };
   return (
     <div>
       {/*  */}
       <NewExpense onAddExpense={addExpenseHandler} />
       {/* Passes expenseArray data as a props "items" to ExpensesList.js */}
-      <ExpensesList items={expensesArray} />
+      <ExpensesList items={expenses} />
     </div>
   );
 };
