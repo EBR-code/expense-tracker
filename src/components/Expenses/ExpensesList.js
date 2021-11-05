bbd0acd271fdb15f98f14f90c0981efd4df64722
Edson Ragas <ragasedson@gmail.com>
2021-11-05 19:46:57 +0800

    add year filter function

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -5,13 +5,18 @@ import ExpensesFilter from './ExpensesFilter';
 import './ExpensesList.css';
 
 //from App.js we pass the expenseArray throught props "items"
-const ExpensesList = props => {
+const ExpensesList = (props) => {
   const [yearChange, setYearChange] = useState('2021');
   // a function that handles the data from the onYearChange props, by setting that data to setYearChange
-  const saveYearChangeHandler = chosenYear => {
+  const saveYearChangeHandler = (chosenYear) => {
     setYearChange(chosenYear);
-    console.log(chosenYear);
+    console.log(yearChange);
+    console.log(setYearChange);
   };
+  // a function that filters by the year cho
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === yearChange;
+  });
   return (
     <div>
       <Card className="expenses-list">
@@ -20,8 +25,9 @@ const ExpensesList = props => {
           onYearChange={saveYearChangeHandler}
         />
         {/* iterate through expense-data-array, pass elements to ExpenseItem attribute */}
-        {props.items.map(expense => (
+        {filteredExpenses.map((expense, index) => (
           <ExpenseItem
+            key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
